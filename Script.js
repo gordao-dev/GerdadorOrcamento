e75eb536c85fa890f526bbfdf7b63b7692cadcdf
gordao-dev <gmdsbr98@gmail.com>
2022-05-17 21:29:25 -0300

    const and preventDefault

---
+++ Script.js
@@ -0,0 +1,7 @@
+const nome = document.getElementById("nome").value;
+const data = document.getElementById("data do evento").value;
+const convidados = document.getElementById("convidados").value;
+
+addEventListener("submit", (e) => {
+    e.preventDefault();
+}); 
