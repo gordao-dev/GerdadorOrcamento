1b21c434da11893af58f5840eafc60828043e09e
gordao-dev <contato.gmds@gmail.com>
2022-07-10 15:56:57 -0300

    live server

--- Script.js
+++ Script.js
@@ -1,7 +1,20 @@
 const nome = document.getElementById("nome").value;
-const data = document.getElementById("data do evento").value;
+const data = document.getElementById("data_do_evento").value;
 const convidados = document.getElementById("convidados").value;
 
 addEventListener("submit", (e) => {
     e.preventDefault();
-}); 
+});
+
+let radioValue = '';
+
+document.querySelector('#gerar').onclick = function () {   
+    var tipoCardapio = document.getElementsByName('contact');
+    for (var radio of tipoCardapio) {
+        if (radio.checked) {
+            radioValue = radio.value;
+        }
+    }
+
+    console.log(radioValue)
+}
